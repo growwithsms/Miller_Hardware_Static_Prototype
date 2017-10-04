fb097488b2f147e49ce96ac34f9f41aaa6290f76
Stefen <the@stefenphelps.com>
2017-10-04 16:39:15 -0400

    add category page

--- js/miller_2017.js
+++ js/miller_2017.js
@@ -1,53 +1,58 @@
-// Home Page announcements
-var current = 0,
-    slides = document.getElementsByClassName('featured-announcement');
 
-setInterval(function() {
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('active');
-    }
-    current = (current != slides.length - 1) ? current + 1 : 0;
-    slides[current].classList.add('active');
-}, 6000);
+// Home Page Specific Scripts
+if( $('.home-page').length ) {
+    // Home Page announcements
+    var current = 0,
+        slides = document.getElementsByClassName('featured-announcement');
+
+    setInterval(function() {
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove('active');
+        }
+        current = (current != slides.length - 1) ? current + 1 : 0;
+        slides[current].classList.add('active');
+    }, 6000);
 
 
-// Home Page Carousels
-var featuredPromotions = document.querySelector('.featured-promotions');
-var promotions = new Flickity(featuredPromotions, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    imagesLoaded: true,
-    prevNextButtons: true,
-    pageDots: false,
-    arrowShape: {
-		x0: 25,
-		x1: 60, y1: 30,
-		x2: 65, y2: 20,
-		x3: 40
-	}
-});
+    // Home Page Carousels
+    var featuredPromotions = document.querySelector('.featured-promotions');
+    var promotions = new Flickity(featuredPromotions, {
+        // options
+        cellAlign: 'left',
+        contain: true,
+        imagesLoaded: true,
+        prevNextButtons: true,
+        pageDots: false,
+        arrowShape: {
+    		x0: 25,
+    		x1: 60, y1: 30,
+    		x2: 65, y2: 20,
+    		x3: 40
+    	}
+    });
 
-var featuredBrands = document.querySelector('.featured-brands');
-var brands = new Flickity(featuredBrands, {
-    // options
-    cellAlign: 'left',
-    imagesLoaded: true,
-    prevNextButtons: true,
-    pageDots: false,
-    groupCells: true,
-    arrowShape: {
-		x0: 25,
-		x1: 60, y1: 30,
-		x2: 65, y2: 20,
-		x3: 40
-	}
-});
+    var featuredBrands = document.querySelector('.featured-brands');
+    var brands = new Flickity(featuredBrands, {
+        // options
+        cellAlign: 'left',
+        imagesLoaded: true,
+        prevNextButtons: true,
+        pageDots: false,
+        groupCells: true,
+        arrowShape: {
+    		x0: 25,
+    		x1: 60, y1: 30,
+    		x2: 65, y2: 20,
+    		x3: 40
+    	}
+    });
 
-// Map Tabs
-$('.map-tabs').on('click', 'a', function(e){
-	e.preventDefault();
-	var $mapPanel = $(this).attr('href');
-	$('.map-panel').removeClass('active');
-	$($mapPanel).addClass('active');
-});
+    // Map Tabs
+    $('.map-tabs').on('click', 'a', function(e){
+    	e.preventDefault();
+    	var $mapPanel = $(this).attr('href');
+    	$('.map-panel').removeClass('active');
+    	$($mapPanel).addClass('active');
+    });
+
+}
